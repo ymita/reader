ca44f77e154f20aa36fee2fbf7fc660a3a2a52aa
ymita <ymita0617@gmail.com>
2018-10-21 18:43:10 +0900

    読み上げ順序 + 著作権追加 + 単語追加
    読み上げ順序 → Promise で修正
    著作権追加 → 著作権表示を追加
    単語追加 → 単語を追加

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let data = [
         "id": 1,
         "spelling": "ameliorate",
         "meaning": "改善する",
-        "text": "It is not clear what can be done to ameliorate the situation."
+        "text": "They offered some compromises in an effort to ameliorate the situation."
     },
     {
         "id": 2,
@@ -11,6 +11,36 @@ let data = [
         "meaning": "萎縮",
         "text": "Atrophy is the partial or complete wasting away of a part of the body."
     },
+    {
+        "id": 3,
+        "spelling": "precinct",
+        "meaning": "選挙区、境界",
+        "text": "A precinct is a space enclosed by the walls or other boundaries of a particular place or building, or by an arbitrary and imaginary line drawn around it."
+    },
+    {
+        "id": 4,
+        "spelling": "allay",
+        "meaning": "静める、和らげる",
+        "text": "Usually you can allay the negativity by providing (or asking members to provide) clear and concise instructions and documentation."
+    },
+    {
+        "id": 5,
+        "spelling": "endemic",
+        "meaning": "特有の、土着の",
+        "text": "This isn't a platform thing, it's a problem that is endemic to the web."
+    },
+    {
+        "id": 6,
+        "spelling": "hydrolysis",
+        "meaning": "加水分解",
+        "text": "Usually hydrolysis is a chemical process in which a molecule of water is added to a substance. Sometimes this addition causes both substance and water molecule to split into two parts."
+    },
+    {
+        "id": 7,
+        "spelling": "debacle",
+        "meaning": "総崩れ、大失敗",
+        "text": "These include the White House’s U.S. Digital Service 18F taskforce, launched in response to the healthcare.gov debacle."
+    }
 ];
 
 // SpeechSynthesisUtterance オブジェクト (例文読み上げに利用する。)
@@ -52,15 +82,72 @@ function createMessage() {
     msg.lang = 'en-US';
 }
 
+// 例文データの長さ (読み上げ終了時に読み上げ位置を初期化するために利用)
+let dataCount = data.length;
+// アクティブアイテムのインデックス情報 (アクティブとしてハイライトするために利用)
+let currentIndex = 0;
+
+// 例文読み上げ、及びリストのスタイリングなど
+function readText(_word) {
+    return function () {
+        return new Promise(function (resolve, reject) {
+            // 読み上げ中のアイテム (読み上げ開始時に、読み上げ中の例文に含まれる学習単語の日本語を表示するために利用)
+            let wordInPronounce;
+
+            // 読み上げ開始時に呼び出されるイベント
+            msg.onstart = (event) => {
+                currentIndex = currentIndex + 1;
+                let anchor = document.querySelectorAll(`[data-index-number="${currentIndex}"]`)[0];
+                anchor.classList.add('active');
+                wordInPronounce = data.find((item) => {
+                    return item.id === currentIndex;
+                });
+
+                // 例文を読み上げ始めたら、画面上に日本語の意味を表示する。
+                if (wordInPronounce) {
+                    document.getElementById('meaning').textContent = wordInPronounce.meaning;
+                }
+            }
+
+            // 読み上げ終了時に呼び出されるイベント
+            msg.onend = (event) => {
+                let anchor = document.querySelectorAll(`[data-index-number="${currentIndex}"]`)[0];
+                if (anchor.classList.contains('active')) {
+                    anchor.classList.remove('active');
+                }
+                // 例文を読み上げ終わったら、画面上に表示されている日本語の意味をクリアする。
+                document.getElementById('meaning').textContent = "　";
+
+                if (dataCount === currentIndex) {
+                    // 読み上げを終了すると、再度読み上げられるようにインデックスを初期値に設定
+                    currentIndex = 0;
+                }
+                // 読み上げが終わったら、プロミスを解決
+                resolve(_word);
+            }
+            msg.text = _word.text;
+            // 読み上げ！
+            speechSynthesis.speak(msg);
+        });
+    };
+}
+let start = (evt) => {
+    // 読み上げ処理を保存する配列
+    var promises = [];
+    // 配列に読み上げ処理を追加していく
+    for (let i = 0; i < data.length; i++) {
+        promises.push(readText(data[i]));
+    }
+    // 読み上げ処理を直列実行
+    promises.reduce(function (prev, curr, index, array) {
+        return prev.then(curr);
+    }, Promise.resolve());
+}
+
 // ボタンクリックなどイベント登録用の関数
 function registerEvents() {
     let startButton = document.getElementById('startButton');
-    startButton.addEventListener('click', (evt) => {
-        for (let i = 0; i < data.length; i++) {
-            let text = data[i].text;
-            speech(data[i]);
-        }
-    });
+    startButton.addEventListener('click', start);
 
     let pauseButton = document.getElementById('pauseButton');
     pauseButton.addEventListener('click', (evt) => {
@@ -74,53 +161,9 @@ function registerEvents() {
     endButton.addEventListener('click', (evt) => {
         speechSynthesis.cancel();
     });
-    
-    
-}
 
-// 例文データの長さ (読み上げ終了時に読み上げ位置を初期化するために利用)
-let dataCount = data.length;
-// アクティブアイテムのインデックス情報 (アクティブとしてハイライトするために利用)
-let currentIndex = 0;
-
-// 例文読み上げ、及びリストのスタイリングなど
-function speech(word) {
-
-    // 読み上げ中のアイテム (読み上げ開始時に、読み上げ中の例文に含まれる学習単語の日本語を表示するために利用)
-    let wordInPronounce;
-
-    // 読み上げ開始時に呼び出されるイベント
-    msg.onstart = (event) => {
-        currentIndex = currentIndex + 1;
-        let anchor = document.querySelectorAll(`[data-index-number="${currentIndex}"]`)[0];
-        anchor.classList.add('active');
-        wordInPronounce = data.find((item) => {
-            return item.id === currentIndex;
-        });
-
-        // 例文を読み上げ始めたら、画面上に日本語の意味を表示する。
-        if (wordInPronounce) {
-            document.getElementById('meaning').textContent = wordInPronounce.meaning;
-        }
-    }
-
-    // 読み上げ終了時に呼び出されるイベント
-    msg.onend = (event) => {
-        let anchor = document.querySelectorAll(`[data-index-number="${currentIndex}"]`)[0];
-        anchor.classList.remove('active');
-
-        // 例文を読み上げ終わったら、画面上に表示されている日本語の意味をクリアする。
-        document.getElementById('meaning').textContent = "　";
-
-        if (dataCount === currentIndex) {
-            // 読み上げを終了すると、再度読み上げられるようにインデックスを初期値に設定
-            currentIndex = 0;
-        }
-    }
-    msg.text = word.text;
-    // 読み上げ！
-    speechSynthesis.speak(msg);
 
 }
+
 
 init();
