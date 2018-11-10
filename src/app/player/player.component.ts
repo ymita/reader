084efd8a8edef078f9d7bf756b9175ff9cdc1025
ymita <ymita0617@gmail.com>
2018-11-10 14:01:23 +0900

    misc update

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -19,7 +19,9 @@ export class PlayerComponent implements OnInit {
     private changeDetectorRef: ChangeDetectorRef
   ) { }
 
-  async ngOnInit() {
+  ngOnInit() {
+    
+    // sentenceService.getData() 呼び出し後にデータを受け取る
     const sub = this.sentenceService.getCurrentData();
     sub.subscribe((_words: Word[]) => {
       this.sentences = _words;
@@ -27,7 +29,7 @@ export class PlayerComponent implements OnInit {
 
     this.sentenceService.getData();
 
-    let subscription = this.sentenceService.getCurrentSentence();
+    const subscription = this.sentenceService.getCurrentSentence();
     subscription.subscribe((_word: Word) => {
       this.id = _word.id;
       this.meaning = _word.meaning;
